cd7dd80c853a001615fc4a399a48289c97a0f97a
SYNAPSE\amit.ganvir <amit.ganvir@globallogi.com>
2018-11-15 19:17:54 +0530

    kube git2consul

--- minishift/consul/cluster-with-Token/gitconsul-docker-create.sh
+++ minishift/consul/cluster-with-Token/gitconsul-docker-create.sh
@@ -1,7 +1,7 @@
 namespace=glt-consul
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-git clone https://github.com/amitganvir23/kubernetes-minishift-openshift.git
-cd kubernetes-minishift-openshift/minishift/consul/cluster-with-Token/git2consul-docker-image/
-if [ ! -f id_rsa ];then echo File is missing id_rsa;pwd;exit 0;fi
-chmod 600 id_rsa
-docker build -t amitrepo/git2consul2:0.12.13 .
+minikube ssh "ssh-keyscan github.com >> ~/.ssh/known_hosts; \
+git clone https://github.com/amitganvir23/kubernetes-minishift-openshift.git; \
+cd kubernetes-minishift-openshift/minishift/consul/cluster-with-Token/git2consul-docker-image/; \
+if [ ! -f id_rsa ];then echo File is missing id_rsa;pwd;exit 0;fi; \
+chmod 600 id_rsa; \
+docker build -t amitrepo/git2consul:0.12.13 ."
